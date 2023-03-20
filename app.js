7aa59841831869618b276c28500f89ca2b4818e3
Paulfresnel <paul.fresnel1@gmail.com>
2023-03-20 11:43:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require("./db");
 // https://www.npmjs.com/package/express
 const express = require("express");
 
+//comment to push & delete
+
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
